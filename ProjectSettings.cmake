d5068b3cc79ab13f2c2155545c59936135a5ed41
Nuno Barros <barros@lip.pt>
2024-09-27 12:27:40 +0200

    Fixed project linking and library dependency

--- ProjectSettings.cmake
+++ ProjectSettings.cmake
@@ -6,11 +6,12 @@
 #set(CUSTOM_SERVER_MODULES AccessControl)
 set(CUSTOM_SERVER_MODULES LaserControl LaserUtils)
 include_directories( ${PROJECT_SOURCE_DIR}/LaserUtils/i2c/ )
+include_directories( ${PROJECT_SOURCE_DIR}/LaserUtils/common/ )
 #include_directories( ${PROJECT_SOURCE_DIR}/LaserUtils/i2c/ )
 
 set(EXECUTABLE OpcUaServer)
 set(SERVER_INCLUDE_DIRECTORIES  )
-set(SERVER_LINK_LIBRARIES  "-lcurl" spdlog i2c)
+set(SERVER_LINK_LIBRARIES  "-lcurl" spdlog i2c cib_i2c)
 set(SERVER_LINK_DIRECTORIES  )
 
 add_compile_definitions(SIMULATION)
