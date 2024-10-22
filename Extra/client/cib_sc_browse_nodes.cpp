126e48b27ed73e007458986fa602fd7f3186f0f4
Nuno Barros <barros@lip.pt>
2024-10-22 11:18:47 +0100

    Cleaning up code.

--- Extra/client/cib_sc_browse_nodes.cpp
+++ Extra/client/cib_sc_browse_nodes.cpp
@@ -184,7 +184,7 @@ void browse_nodes_scan_cib(UA_Client *client, UA_NodeId *node)
         {
           // this is an argument
           // try to fetch its data type exactly
-          printf("\t\t\t\t %-16.*s \n",
+          printf("\t\t %-16.*s \n",
                  (int)ref->browseName.name.length, ref->browseName.name.data
                  );
           //(int)ref->browseName.name.length, ref->browseName.name.data,
@@ -193,7 +193,7 @@ void browse_nodes_scan_cib(UA_Client *client, UA_NodeId *node)
         {
         // query the data type
         //UA_Client_readValueAttribute(UA_Client *client, const UA_NodeId nodeId, UA_Variant *outValue)
-          printf("%-16.*s --> VAR : %s \n",
+          printf("%-16.*s\tVAR\t%s \n",
                  (int)ref->nodeId.nodeId.identifier.string.length,
                  ref->nodeId.nodeId.identifier.string.data,
                  output.type->typeName
@@ -210,19 +210,18 @@ void browse_nodes_scan_cib(UA_Client *client, UA_NodeId *node)
       }
       else if (ref->nodeClass == UA_NODECLASS_OBJECTTYPE)
       {
-        printf("\t\t\t\t%-16.*s --> %-16.*s\n",
-               (int)ref->browseName.name.length,
-               ref->browseName.name.data,
-               (int)ref->nodeId.nodeId.identifier.string.length,
-               ref->nodeId.nodeId.identifier.string.data
-
-        );
+//        printf("\t\t\t\t%-16.*s --> %-16.*s\n",
+//               (int)ref->browseName.name.length,
+//               ref->browseName.name.data,
+//               (int)ref->nodeId.nodeId.identifier.string.length,
+//               ref->nodeId.nodeId.identifier.string.data
+//        );
       }
       else if (ref->nodeClass == UA_NODECLASS_METHOD)
       {
         // in this case we need to figure out whether they have arguments
         // so we should also fetch their arguments
-        printf("%-16.*s --> METHOD \n",
+        printf("%-16.*s\tMETHOD\n",
                (int)ref->nodeId.nodeId.identifier.string.length,
                ref->nodeId.nodeId.identifier.string.data
                );
@@ -237,7 +236,7 @@ void browse_nodes_scan_cib(UA_Client *client, UA_NodeId *node)
       }
       else
       {
-        printf("%-16.*s --> %d \n",
+        printf("%-16.*s\t%d\n",
                (int)ref->nodeId.nodeId.identifier.string.length,
                ref->nodeId.nodeId.identifier.string.data,
                (int)ref->nodeClass
