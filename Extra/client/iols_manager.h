c3e7ed8b3ac6056b68168c8a72b9acf4d88d73a9
Nuno Barros <barros@lip.pt>
2024-10-25 14:50:33 +0100

    Adding missing prototype

--- Extra/client/iols_manager.h
+++ Extra/client/iols_manager.h
@@ -13,6 +13,7 @@ void write_to_pane(WINDOW *pane, int y, int x, const std::string &text);
 void set_label_color(WINDOW *pane, int y, int x, const std::string &label, const std::string &status);
 void reset_right_pane(WINDOW *right_pane);
 void update_feedback(const std::string &msg);
+void update_feedback_json(json &entry);
 void update_right_pane(WINDOW *right_pane, std::atomic<bool> &running, int height, iols_monitor_t &status);
 void refresh_left_panel(WINDOW *pane, int height);
 
