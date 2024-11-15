a837b8eeb3d6e3dbd65969292ec336c90fd3e180
Nuno Barros <barros@lip.pt>
2024-11-15 15:56:19 +0000

    Fixed a bug in execute_grid_scan

--- Extra/client/IoLSMonitor.cpp
+++ Extra/client/IoLSMonitor.cpp
@@ -729,7 +729,7 @@ bool IoLSMonitor::execute_grid_scan(const std::string &run_plan, FeedbackManager
       return false;
     }
 
-    if (!jrun_plan.contains("scan_axis") || !jrun_plan["scan_axis"].is_number_unsigned() || jrun_plan["scan_axis"].get<std::string>().size() > 2)
+    if (!jrun_plan.contains("scan_axis") || !jrun_plan["scan_axis"].is_number_unsigned() || jrun_plan["scan_axis"].get<int>() > 2)
     {
       feedback.add_message(Severity::ERROR, "Invalid or missing 'scan_axis' key. Expected an unsigned integer < 2.");
       return false;
